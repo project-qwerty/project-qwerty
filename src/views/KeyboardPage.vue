bb312cc8aa329836af0c11f47e7ca5ea602c5da6
Eliza <ercri1@student.monash.edu>
2020-02-05 14:51:03 +1100

    Errorless controlled by Settings
    The Errorless cookie is now imported into the Keyboard page, allowing the setting to control errorless on or off.

--- src/views/KeyboardPage.vue
+++ src/views/KeyboardPage.vue
@@ -13,6 +13,105 @@
 </template>
 
 
+<script>
+  import Keyboard from '../components/keyboard/KeyboardComponent';
+  import WordList from '../components/WordList';
+  import Modal from 'vue-modal';
+
+  export default {
+    name: 'app',
+    components: {
+      WordList,
+      Keyboard,
+      Modal
+    },
+    data() {
+      return {
+        showModal:true,
+        wordlist: ["hello"],
+        index: 0,
+        output: "",
+        isHidden: false,
+        errorlessOnOff: true, // Controls whether errorless is on or off 
+        timerOnOff: false, // Controls whether the timer is on or off (line 168)
+        correct_audio : new Audio(require('@/assets/correct.mp3')),
+          settings: {
+              timer : 1
+          }
+      }
+    },
+    //Run on loading of page
+    created() {
+      // Add all the cookies here (first two lines are the important ones)
+      //Timer Control
+      if(this.$cookies.isKey('settings.timer')){
+        this.timer = this.$cookies.get('settings.timer');
+        if(this.$cookies.get('settings.timer') == 0) {
+          this.timerOnOff = false;
+        } else {
+          this.timerOnOff = true;
+        }
+      }
+      setTimeout(this.hide, this.timer * 1000);
+      //Errorless Control
+      if(this.$cookies.isKey('settings.errorless')){
+        this.timer = this.$cookies.get('settings.errorless');
+        if(this.$cookies.get('settings.errorless') == 'ON') {
+          this.errorlessOnOff = true;
+        } else {
+          this.errorlessOnOff = false;
+        }
+      }
+      //Repetition Control
+      //Starter Mode Control
+      //Keyboard Control
+      //Word Number Control
+    },
+    computed : {
+      'word' : function(){
+        if(this.errorlessOnOff) return this.wordlist[this.index][this.output.length];
+        return 'abcdefghijklmnopqrstuvwxyz backspace'
+      }
+    },
+    methods: {
+      keypressed(char) {
+        if (char !== "backspace") {
+          if (this.errorlessOnOff) {
+            var _word = this.wordlist[this.index];
+            if (char === _word[this.output.length]) {
+              this.output += char;
+            } 
+          } else {
+            this.output += char;
+          }
+        } else {
+          this.output = this.output.substring(0, this.output.length - 1);
+        }
+        if (this.output === this.wordlist[this.index]) {
+          this.index += 1;
+          this.output = "";
+          this.correct_audio.play();
+          
+          // Ths is the timer setting, time is in milliseconds
+            // This should be where the if statement checking if the timer is set to zero should be added.
+          this.isHidden = false;
+          setTimeout(this.hide, this.timer * 1000);
+          if (this.index == this.wordlist.length) {
+            this.index = 0;
+            this.output = "";
+            this.wordlist = ["You Win!"];
+            
+          }
+        }
+      },
+      hide() {
+        this.isHidden = this.timerOnOff;
+      }
+    }, 
+  }
+</script>
+
+
 <style>
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -54,88 +153,3 @@
     height: 100px;
   }
 </style>
-
-
-<script>
-  import Keyboard from '../components/keyboard/KeyboardComponent';
-  import WordList from '../components/WordList';
-  import Modal from 'vue-modal';
-
-  export default {
-    name: 'app',
-    components: {
-      WordList,
-      Keyboard,
-      Modal
-    },
-    data() {
-      return {
-        showModal:true,
-        wordlist: ["hello"],
-        index: 0,
-        output: "",
-        isHidden: false,
-        errorless: false, // Controls whether errorless is on or off 
-        timerOnOff: false, // Controls whether the timer is on or off (line 168)
-        correct_audio : new Audio(require('@/assets/correct.mp3')),
-          settings: {
-              timer : 1
-          }
-      }
-    },
-    //Run on loading of page
-    created() {
-      // Add all the cookies here (first two lines are the important ones)
-      if(this.$cookies.isKey('settings.timer')){
-        this.timer = this.$cookies.get('settings.timer');
-        if(this.$cookies.get('settings.timer') == 0) {
-          this.timerOnOff = false;
-        } else {
-          this.timerOnOff = true;
-        }
-      }
-      setTimeout(this.hide, this.timer * 1000);
-    },
-    computed : {
-      'word' : function(){
-        if(this.errorless) return this.wordlist[this.index][this.output.length];
-        return 'abcdefghijklmnopqrstuvwxyz backspace'
-      }
-    },
-    methods: {
-      keypressed(char) {
-        if (char !== "backspace") {
-          if (this.errorless) {
-            var _word = this.wordlist[this.index];
-            if (char === _word[this.output.length]) {
-              this.output += char;
-            } 
-          } else {
-            this.output += char;
-          }
-        } else {
-          this.output = this.output.substring(0, this.output.length - 1);
-        }
-        if (this.output === this.wordlist[this.index]) {
-          this.index += 1;
-          this.output = "";
-          this.correct_audio.play();
-          
-          // Ths is the timer setting, time is in milliseconds
-            // This should be where the if statement checking if the timer is set to zero should be added.
-          this.isHidden = false;
-          setTimeout(this.hide, this.timer * 1000);
-          if (this.index == this.wordlist.length) {
-            this.index = 0;
-            this.output = "";
-            this.wordlist = ["You Win!"];
-            
-          }
-        }
-      },
-      hide() {
-        this.isHidden = this.timerOnOff;
-      }
-    }, 
-  }
-</script>
