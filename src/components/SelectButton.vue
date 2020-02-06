1831c4d52ecaf0ec6b1d8da361071139460bfd13
Eliza <ercri1@student.monash.edu>
2020-02-06 12:22:36 +1100

    Select Buttons on Select List Page

---
+++ src/components/SelectButton.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="btn-container">
+    <button v-on:click="toggle=!toggle" :class="{'selected':toggle===true}"></button>
+  </div>
+</template>
+
+
+<script>
+  export default{
+    name: "select-component",
+    props: ["options", "preset"],
+    data: function(){
+      return{
+        toggle: false,
+      }
+    },
+    created(){
+      this.toggle = this.preset  
+    },
+    watch: {
+      'toggle' : function(val){
+        this.$emit("update:value", val)
+      }
+    }
+  }
+</script>
+
+
+<style scoped>
+  .selected{ 
+    background-color: rgb(48, 209, 88) !important /*Makes this the*/
+  }
+  
+  .btn-container{
+    display: flex;
+    align-items: stretch;
+  }
+
+  button{
+    background-color: #fefefe; 
+    color: #212121;
+    height: 28px;
+    width: 28px;
+    padding-top:8px;
+    padding-bottom:8px;
+    outline: none;
+    transition: all 200ms;
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  }
+</style>
--- src/views/SelectWords.vue
+++ src/views/SelectWords.vue
@@ -1,13 +1,49 @@
 <template>
   <div>
-    <router-link style="display:flex;justify-content:center;align-items:center;text-decoration:none" to="/settings">
+    <router-link style="display:flex;justify-content:left;align-items:center;text-decoration:none" to="/settings">
       <p style="text-align:center;font-size:36px;margin-right:16px;color:#414141">Settings</p>
       <font-awesome-icon style="font-size:100;color:#414141" icon="cog"/>
     </router-link>
-    select words
-    <router-link style="display:flex;justify-content:center;align-items:center;text-decoration:none" to="/keyboard">
-      <p style="text-align:center;font-size:36px;margin-right:16px;color:#414141">Start</p>
+    <p class=heading>Select Word List</p>
+    
+     <p class="list-heading">List A</p>
+        <select-component :options="['']"/>
+    
+    <p class="list-heading">List B</p>
+        <select-component :options="['']"/>
+    
+    <router-link style="display:flex;justify-content:center;align-items:center;text-decoration:none;margin-left:100px" to="/keyboard">
+      <p style="text-align:center;font-size:36px;margin-right:16px:16px;color:#414141">Start</p>
       <font-awesome-icon style="font-size:100;color:#414141" icon="chevron-right"/>
     </router-link>
   </div>
 </template>
+
+<script>
+ import SelectButton from '@/components/SelectButton.vue';
+  
+export default {
+    data() {
+      return {
+        token: null,
+      }
+    },
+ 
+    components: {
+      'select-component' : SelectButton
+    },
+  }
+</script>
+
+
+<style scoped>
+  .heading {
+    font-size:28px;
+    text-align: left;
+  }
+  
+  .list-heading {
+    font-size:20px;
+    text-align: left;
+  }
+</style>
