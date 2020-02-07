5b28d13893d74248c78c2b9d167829c94a338fc4
Eliza <ercri1@student.monash.edu>
2020-02-07 16:42:00 +1100

    Start Button Improved

--- src/components/StartButton.vue
+++ src/components/StartButton.vue
@@ -1,8 +1,8 @@
 <template>
-  <div style="border:1px solid black;width:225px;margin:auto">
+  <div style="width:225px; margin:auto" class="box">
     <router-link style="display:flex;justify-content:center;align-items:center;text-decoration:none" :to="to" >
-      <p style="text-align:center;font-size:36px;margin-right:16px;color:#414141">Start</p>
-      <font-awesome-icon style="font-size:100;color:#414141" icon="chevron-right"/>
+      <p style="text-align:center;font-size:36px;margin-right:16px;color:black">START</p>
+      <font-awesome-icon style="font-size:100;color:rgba(142, 142, 147)" icon="chevron-right"/>
     </router-link>
   </div>
 </template>
@@ -14,3 +14,22 @@
     props: ["to"],
   }
 </script>
+
+
+<style>
+  .box {
+    background-color: white;
+    border: 2px solid rgba(99, 99, 102, 0.5);
+    text-transform: capitalize;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 24px;
+/*    width: 100px;*/
+    height: 100%;
+    border-radius: 2px;
+    margin: 0px 6px;
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.5);
+  }
+</style>
