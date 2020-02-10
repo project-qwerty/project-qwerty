59d6a455ad5f0fae171d031bd181e194b86c7abc
Tim <tjhar15@student.monash.edu>
2020-02-10 13:50:18 +1100

    Preparation for meeting with aphasia group

--- src/views/settings/CustomLists.vue
+++ src/views/settings/CustomLists.vue
@@ -1,6 +1,7 @@
 <template>
   <div style="width:600px; padding:48px;text-align:left">
     <h1 style="font-weight:200">Custom Lists</h1>
-    
+    <img alt="MIME logo" style="height:550px" :src="require('@/assets/MTP/custom_lists.png')">
+    <p>This page is currently non-funcitonal</p>
   </div>
 </template>
