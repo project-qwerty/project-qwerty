29c8d759e6b8eb156424b60a8ae1d4ab02091e83
Eliza <ercri1@student.monash.edu>
2020-02-17 10:23:23 +1100

    Start Button
    Start Button Enlarged and Made Green

--- src/components/StartButton.vue
+++ src/components/StartButton.vue
@@ -1,8 +1,8 @@
 <template>
-  <div style="width:225px; margin:auto" class="box">
+  <div style="width:460px; margin:auto" class="box">
     <router-link style="display:flex;justify-content:center;align-items:center;text-decoration:none" :to="to" >
-      <p style="text-align:center;font-size:36px;margin-right:16px;color:black">START</p>
-      <font-awesome-icon style="font-size:100;color:rgba(142, 142, 147)" icon="chevron-right"/>
+      <p style="text-align:center;font-size:40px;margin-right:16px;color:black">START</p>
+      <font-awesome-icon style="font-size:100;color:black" icon="chevron-right"/>
     </router-link>
   </div>
 </template>
@@ -18,18 +18,16 @@
 
 <style>
   .box {
-    background-color: white;
+    background-color: rgb(48, 209, 88);
     border: 2px solid rgba(99, 99, 102, 0.5);
     text-transform: capitalize;
     box-sizing: border-box;
     display: flex;
     align-items: center;
     justify-content: center;
-    font-size: 24px;
-/*    width: 100px;*/
     height: 100%;
     border-radius: 2px;
     margin: 0px 6px;
-    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.5);
+    box-shadow: 0 10px 10px -1px rgba(0, 0, 0, 0.5);
   }
 </style>
