0d1df3a6a0311813e9b0f36e17f36fd131e8052d
Eliza <ercri1@student.monash.edu>
2020-02-20 13:55:18 +1100

    Added More Inbuilt Words

--- src/components/InbuiltWordlists.js
+++ src/components/InbuiltWordlists.js
@@ -2,4 +2,5 @@ export default [
   {"Email": ["hello", "to", "dear", "sincerely", "reply", "respond", "action", "immediately", "manager", "team"]},
   {"Text Messages": ["hey", "hi", "thanks", "cheers", "love", "day", "night", "tonight", "today", "tomorrow", "yesterday", "sorry", "ok", "omg", "haha"]},
   {"Text Messages": ["hey", "hi", "thanks", "cheers", "love", "day", "night", "tonight", "today", "tomorrow", "yesterday", "sorry", "ok", "omg", "haha"]},
+  {"Groceries": ["shopping", "supermarket", "checkout", "scan", "cash"]},
 ]
