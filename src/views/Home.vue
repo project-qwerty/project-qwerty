730a81cf4b7ef31711fb4bdb43d2f405b73b8f7a
Eliza <ercri1@student.monash.edu>
2020-02-25 13:15:50 +1100

    Alternative Logos
    Logo 3 is now used

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,9 @@
   <div class="home">
     <Header />
     <div style="opacity:0;height:50px"></div>
-    <img alt="App logo" style="height:400px" src="../assets/app_logo.png">
-    <Start :to="to" />
+    <img alt="App logo" style="height:320px" src="../assets/app_logo_3.png">
+    <p class="paragraph"> This app is for people with aphasia after a stroke </p>
+    <Start :to="to" style="float:right; margin:50px"/>
   </div>
 </template>
 
@@ -25,3 +26,13 @@
     }
   }
 </script>
+
+
+<style>
+  .paragraph {
+    float: left;
+    margin: 50px;
+    font-size: 32px;
+    color:rgba(58, 58, 60);
+  }
+</style>
