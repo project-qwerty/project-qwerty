2a36e682c64703f6fe0861f8ee0a846d999c31a0
Tim <tjhar15@student.monash.edu>
2020-07-23 13:25:46 +1000

    fixed email 2

--- src/views/settings/About.vue
+++ src/views/settings/About.vue
@@ -3,7 +3,7 @@
     <h1 style="font-weight:200">About</h1>
     <p>
       This is an app for people with aphasia.<br>
-      If you notice any bugs and would like to report them, please email projectqwerty@gmail.com
+      If you notice any bugs and would like to report them, please email projectqwerty2020@gmail.com
     </p>
   </div>
 </template>
