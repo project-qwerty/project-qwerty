edbe0a7c46d653658f0549fa0b637cd2d17e6286
Eliza <ercri1@student.monash.edu>
2020-07-26 18:50:09 +1000

    About Page (Edit)

--- src/views/settings/About.vue
+++ src/views/settings/About.vue
@@ -2,11 +2,11 @@
   <div style="padding-left:48px;text-align:left">
     <h1 style="font-weight:200">About</h1>
     <p>
-      This is an app for people with aphasia.<br>
-      It is a work in progress.
+      Welcome to Project QWERTY, this is an app for people with aphasia.<br>
+      This app has been made by Monash University students and Monash Health speech pathologists working alongside people with aphasia. It is a work in progress.
       <br><br>
-      We are currently having issues with Safari but the app seems to work in Google Chrome.<br>
-      If you notice any bugs and would like to report them, please email projectqwerty2020@gmail.com
+      Currently this website works best on Google Chrome, we are working on making this website fully functional on other browsers<br>
+      We would like to make this the best tool possible for people with aphasia. So if you notice any issues or bugs and would like to report them, please email projectqwerty2020@gmail.com.
     </p>
   </div>
 </template>
