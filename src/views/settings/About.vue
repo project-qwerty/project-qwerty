9d4f1333968c56c003de8db5f4acc09b452d889b
Eliza <ercri1@student.monash.edu>
2020-07-28 21:02:03 +1000

    About Page Update
    Adding Information and Basic Formatting to Setting

--- src/views/settings/About.vue
+++ src/views/settings/About.vue
@@ -1,12 +1,40 @@
 <template>
   <div style="padding-left:48px;text-align:left">
     <h1 style="font-weight:200">About</h1>
+    <p class="bold">
+      Welcome to Project QWERTY, this is an app for people with aphasia.
+    </p>
     <p>
-      Welcome to Project QWERTY, this is an app for people with aphasia.<br>
-      This app has been made by Monash University students and Monash Health speech pathologists working alongside people with aphasia. It is a work in progress.
-      <br><br>
-      Currently this website works best on Google, we are working on making this website fully functional on other browsers<br>
+      This app has been made by Monash University students and Monash Health speech pathologists working alongside people with aphasia. It is a work in progress.<br><br>
+      Currently this website works best on Google on iPads and on Google Chrome on other tablet devices, we are working on making this website fully functional on other browsers.<br><br>
       We would like to make this the best tool possible for people with aphasia. So if you notice any issues or bugs and would like to report them, please email projectqwerty2020@gmail.com.
+      <br><br>
+    </p>
+    <p class="bold">
+      Frequent Question and Answers
+    </p>
+    <p>
+      1. Nothing appears when I press start?
+      <br><br>
+      Don't worry, there are a few things you need to do to stop to get the app working Step 1, if you are using an iPads please use Google (not Google Chrome) or if you are using any other tablet you Google Chrome. Step 2, turn off private browsing or incognito mode.
+      <br><br>
+      2. I have a general question about the app?
+      <br><br>
+      No worries, please send use and email at projectqwerty2020@gmail.com.
+      <br><br>
+      3. I have an error I would like to report?
+      <br><br>
+      We are sorry that you are experincing an error, please let us know what's gone wrong by emailing projectqwerty2020@gmail.com. Please include a screenshot and description of the error.
     </p>
   </div>
 </template>
+
+
+
+<style>
+  .bold {
+    font-size: 20px;
+    font-weight: bold;
+    color:rgba(58, 58, 60);
+  }
+</style>
