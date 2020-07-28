30304240cfea8db39a17c1058c14db606a9e6dcc
Eliza <ercri1@student.monash.edu>
2020-07-28 20:33:55 +1000

    iPad Formatting Home Page

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,9 +1,9 @@
 <template>
   <header class="header">
     <div style="display:flex;justify-content:space-between;align-items:center;padding-bottom:30px">
-      <img alt="MIME logo" style="height:80px" :src="require('@/assets/MIME_logo.png')">
-      <img alt="MYMI logo" style="height:80px" :src="require('@/assets/MYMI_logo.png')">
-      <img alt="Monash Partners logo" style="height:80px" :src="require('@/assets/monash-partners-logo.jpg')">
+      <img alt="MIME logo" style="height:60px" :src="require('@/assets/MIME_logo.png')">
+      <img alt="MYMI logo" style="height:60px" :src="require('@/assets/MYMI_logo.png')">
+      <img alt="Monash Partners logo" style="height:60px" :src="require('@/assets/monash-partners-logo.jpg')">
     </div>
   </header>
 </template>
