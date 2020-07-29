2499ecc58f2b52cec98305934567861a11d08582
Tim <tjhar15@student.monash.edu>
2020-07-29 10:24:41 +1000

    Link to about page from home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,15 @@
     <Header />
     <div style="opacity:0;height:20px"></div>
     <img alt="App logo" style="height:260px" src="../assets/projectqwerty1.png">
-    <p class="paragraph"> This app is for people with aphasia after a stroke </p>
-    <div style="width:360px; margin:auto" class="box">
-      <router-link style="display:flex;justify-content:center;align-items:center;text-decoration:none" :to="to" >
-        <p style="text-align:center;font-size:40px;margin-right:16px;color:black">START</p>
-        <font-awesome-icon style="font-size:100;color:black" icon="chevron-right"/>
-      </router-link>
+    <div style="display:flex;justify-content:flex-end">
+      <p class="paragraph"> This app is for people with aphasia after a stroke.<br>Find out more
+        <router-link :to="settings">here</router-link>.</p>
+      <div style="width:360px; margin:auto" class="box">
+        <router-link style="display:flex;justify-content:center;align-items:center;text-decoration:none" :to="to" >
+          <p style="text-align:center;font-size:40px;margin-right:16px;color:black">START</p>
+          <font-awesome-icon style="font-size:100;color:black" icon="chevron-right"/>
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -25,6 +28,7 @@
     data() {
       return {
         to: "/select_words",
+        settings: "/settings/about",
       }
     }
   }
@@ -37,6 +41,7 @@
     margin: 28px;
     font-size: 20px;
     color:rgba(58, 58, 60);
+    text-align: left
   }
   .box {
     background-color: rgb(48, 209, 88);
