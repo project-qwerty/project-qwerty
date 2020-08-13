0a65c8d2e7616ddebd35b4c0cf03c4c76c1f9ec5
Eliza <ercri1@student.monash.edu>
2020-08-13 16:19:10 +1000

    About page information a made aphasia friendly

--- src/views/settings/About.vue
+++ src/views/settings/About.vue
@@ -1,31 +1,32 @@
 <template>
   <div style="padding-left:48px;text-align:left">
     <h1 style="font-weight:200">About</h1>
-    <p class="bold">
-      Welcome to Project QWERTY, this is an app for people with aphasia.
-    </p>
-    <p>
-      This app has been made by Monash University students and Monash Health speech pathologists working alongside people with aphasia. It is a work in progress.<br><br>
-      Currently this website works best on Google on iPads and on Google Chrome on other tablet devices, we are working on making this website fully functional on other browsers.<br><br>
-      We would like to make this the best tool possible for people with aphasia. So if you notice any issues or bugs and would like to report them, please email projectqwerty2020@gmail.com.
-      <br><br>
-    </p>
-    <p class="bold">
-      Frequent Question and Answers
-    </p>
-    <p>
-      1. Nothing appears when I press start?
-      <br><br>
-      Don't worry, there are a few things you need to do to stop to get the app working Step 1, if you are using an iPads please use Google (not Google Chrome) or if you are using any other tablet you Google Chrome. Step 2, turn off private browsing or incognito mode.
-      <br><br>
-      2. I have a general question about the app?
-      <br><br>
-      No worries, please send use and email at projectqwerty2020@gmail.com.
-      <br><br>
-      3. I have an error I would like to report?
-      <br><br>
-      We are sorry that you are experincing an error, please let us know what's gone wrong by emailing projectqwerty2020@gmail.com. Please include a screenshot and description of the error.
-    </p>
+      <p>
+        Welcome to Project QWERTY, this is an <strong>app</strong> for people with <strong>aphasia</strong>
+      </p>
+      <p>Project QWERTY is an app made by Monash University <strong> students </strong> and Monash Health <strong> Speech Pathologists</strong> in partnership with <strong>people with aphasia</strong>. 
+      </p>
+      <p> This app is a <strong> work in progress</strong>. </p>
+      <p>We are <strong> working </strong> on making the app fully functional on all web browsers.</p>
+
+      <h1 style="font-weight:200">Frequently Asked Questions (FAQs)</h1>
+      <p><em><strong>What is </strong>Project QWERTY?</em></p>
+      <p> Project QWERTY is an app for people with <strong> aphasia</strong>. It targets <strong> spelling </strong> and <strong> typing </strong> through the use of pre-programmed word lists and the option to create custom word lists.
+      </p>
+        <br>
+      <p><em>Nothing happens when I press <strong>start</strong>. What do I do?</em></p>
+      <p>1.	If you are using an <strong>iPad</strong>, access this app via the <strong>Google app</strong>.
+      </p><p>OR</p><p>
+      If you are using a <strong>computer</strong> or other <strong>tablet</strong> device, access this app via <strong>Google Chrome</strong>.</p><p>
+      2.Turn <strong> off </strong> private browsing / incognito mode.</p> 
+        <br>
+      <p><em>
+      Who do I contact if I have a <strong>question</strong>  about Project QWERTY? </em></p><p>Please <strong>email</strong> us at projectqwerty2020@gmail.com </p>
+        <br>
+      <p><em>How do I report an <strong>error / bug?</strong></em></p>
+      <p>We are sorry you are experiencing an error. Please <strong>email</strong> us at projectqwerty2020@gmail.com with a <strong>description</strong> and <strong>screenshot</strong> of the error.
+</p>
+
   </div>
 </template>
 
@@ -33,8 +34,7 @@
 
 <style>
   .bold {
-    font-size: 20px;
-    font-weight: bold;
-    color:rgba(58, 58, 60);
+    color:black;
+
   }
 </style>
