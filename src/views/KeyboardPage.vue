43b756b1c0c0b8a6f58cc257d6d75ea7175338e5
William Holt <halbrd@gmail.com>
2021-07-14 09:42:01 +1000

    remove duplicate global #app css declaration

--- src/views/KeyboardPage.vue
+++ src/views/KeyboardPage.vue
@@ -316,12 +316,6 @@
 </script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-  }
 
   body {
     line-height: 1.4;
