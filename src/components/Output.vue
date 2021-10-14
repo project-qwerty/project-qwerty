6d58e0de98b1d97b46794e6ef4dc07c6ad6ed97b
William Holt <halbrd@gmail.com>
2021-10-14 22:45:05 +1100

    make trailing space characters get underlined

--- src/components/Output.vue
+++ src/components/Output.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="output">{{output}}</div>
+  <div class="output">{{renderedOutput}}</div>
 </template>
 
 <script>
   export default {
-    props:[
-      "output",
+    props: [
+      'output',
     ],
+    computed: {
+      'renderedOutput': function() {
+        // \xa0 is the escape code for &nbsp;
+        // there are two of them to make the space characters wider to match the letters better
+        return this.output.replaceAll(' ', '\xa0\xa0');
+      }
+    }
   }
 </script>
 
@@ -16,7 +23,7 @@
     text-transform: uppercase;
     text-decoration: underline;
     height: 100px;
-    color:rgba(28, 28, 30);
-    font-weight:bold;
+    color: rgba(28, 28, 30);
+    font-weight: bold;
   }
 </style>
