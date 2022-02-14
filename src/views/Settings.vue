f65613d939891a1ea7752bdc32b5f9d1b02b585f
William Holt <halbrd@gmail.com>
2022-02-14 18:52:28 +1100

    replace SettingsMenu sidebar with NavSidebar

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,43 +1,26 @@
 <template>
-  <div class="settings">
-    <SettingsMenu :options="options" :name="name" />
-    <div class="settings-page-content">
-      <router-view></router-view>
-    </div>
+  <div>
+    <NavSidebar />
+    <router-view class="page-content" />
   </div>
 </template>
 
 
 <style scoped>
-  .settings {
-    display: flex;
-    height: 100%;
-  }
-
-  .settings-page-content {
-    display: flex;
-    flex-direction: column;
+  .page-content {
+    position: absolute;
+    left: var(--sidebar-width);
   }
 </style>
 
 
 <script>
-  import SettingsMenu from '../components/layout/SettingsMenu';
+  import NavSidebar from '../components/NavSidebar';
 
   export default {
     name: "Settings",
     components: {
-      SettingsMenu,
+      NavSidebar,
     },
-    data: function() {
-      return {
-        // Links to each settings page
-        'options': [
-          {link: '/settings', name: 'General'},
-          {link: '/settings/custom-lists', name: 'Custom Categories'},
-          {link: '/settings/about', name: 'About'},
-        ],
-      }
-    }
   }
 </script>
