a37e9d7b9dd0c41873f3f4111b3bc716765338f8
William Holt <halbrd@gmail.com>
2022-02-16 18:29:52 +1100

    add explanation texts to Settings page

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -9,36 +9,39 @@
     <div class="sidebar-page-content page-wrapper">
       <IconHeader :major="true" text="Settings" icon="gear" iconColour="var(--primary-colour)" />
 
-      <IconHeader text="Timer" icon="stopwatch" />
-      <ShowCurrentSelector :options="[
-          { label: 'Always shown', val: 0, },
-          { label: '5',            val: 5, },
-          { label: '10',           val: 10, },
-          { label: '15',           val: 15, },
-          { label: '20',           val: 20, },
-          { label: '25',           val: 25, },
-          { label: '30',           val: 30, },
-        ]"
-        :initialValue="wordDisplayTime"
-        v-on:update="wordDisplayTime = $event" />
-
       <IconHeader text="Words" icon="list" />
+      <p>How many <strong>words</strong> would you like to practice?</p>
       <ShowCurrentSelector :options="[
-          { label: '5',  val: 5, },
-          { label: '10', val: 10, },
-          { label: '15', val: 15, },
-          { label: '20', val: 20, },
-          { label: '25', val: 25, },
-          { label: '30', val: 30, },
-          { label: '35', val: 35, },
-          { label: '40', val: 40, },
-          { label: '45', val: 45, },
-          { label: '50', val: 50, },
+          { label: '5 words',  val: 5, },
+          { label: '10 words', val: 10, },
+          { label: '15 words', val: 15, },
+          { label: '20 words', val: 20, },
+          { label: '25 words', val: 25, },
+          { label: '30 words', val: 30, },
+          { label: '35 words', val: 35, },
+          { label: '40 words', val: 40, },
+          { label: '45 words', val: 45, },
+          { label: '50 words', val: 50, },
         ]"
         :initialValue="wordsPerSession"
         v-on:update="wordsPerSession = $event" />
 
+      <IconHeader text="Timer" icon="stopwatch" />
+      <p>For how many <strong>seconds</strong> would you like the word to be shown?</p>
+      <ShowCurrentSelector :options="[
+          { label: 'Always shown', val: 0, },
+          { label: '5 seconds',            val: 5, },
+          { label: '10 seconds',           val: 10, },
+          { label: '15 seconds',           val: 15, },
+          { label: '20 seconds',           val: 20, },
+          { label: '25 seconds',           val: 25, },
+          { label: '30 seconds',           val: 30, },
+        ]"
+        :initialValue="wordDisplayTime"
+        v-on:update="wordDisplayTime = $event" />
+
       <IconHeader text="Assistance level" icon="check" />
+      <p>How much <strong>help</strong> would you like?</p>
       <p>{{ assistanceLevelDescriptions[assistanceLevel] }}</p>
       <ShowAllSelector :options="[
             { label: 'Max.', val: 'MAX', },
@@ -49,6 +52,7 @@
           v-on:update="assistanceLevel = $event" />
 
       <IconHeader text="Click for next word" icon="hand-pointer" />
+      <p><strong>Click</strong> or <strong>tap</strong> for the next word</p>
       <ShowAllSelector :options="[
             { label: 'On',  val: true, },
             { label: 'Off', val: false, },
@@ -57,6 +61,7 @@
           v-on:update="clickForNextWord = $event" />
 
       <IconHeader text="Repetitions" icon="repeat" />
+      <p><strong>How many times</strong> would you like to practice each word?</p>
       <ShowAllSelector :options="[
             { label: '1', val: 1, },
             { label: '3', val: 3, },
@@ -65,6 +70,7 @@
           v-on:update="wordRepetitions = $event" />
 
       <IconHeader text="Capitalization" icon="font" />
+      <p>How would you like the words and keyboard to appear?</p>
       <ShowAllSelector :options="[
             { label: 'UPPERCASE', val: 'UPPERCASE', },
             { label: 'lowercase', val: 'LOWERCASE', },
@@ -97,7 +103,7 @@
 
         assistanceLevelDescriptions: {
           'MAX': 'Maximum: The next letter will always be highlighted.',
-          'MIN': 'Minimum: The next letter will be highlighted if a mistake is made.',
+          'MIN': 'Minimum: The next letter will be highlighted if you make a mistake.',
           'NONE': 'None: Letter highlighting is turned off.',
         },
       }
