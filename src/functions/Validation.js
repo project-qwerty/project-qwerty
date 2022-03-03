30d812f3a52f4bc98f137533a10293f80b6be16f
William Holt <halbrd@gmail.com>
2022-03-03 12:41:08 +1100

    make Validation functions accept null inputs

--- src/functions/Validation.js
+++ src/functions/Validation.js
@@ -2,12 +2,20 @@ import LocalStorage from '@/functions/LocalStorage.js'
 
 export default {
   isValidWord(input) {
+    if (typeof input !== 'string') {
+      return false;
+    }
+
     const isAlphaSpace = input.match(/^[a-z ]+$/);
     const isWhitespace = input.replace(/\s/g, '') === '';
 
     return isAlphaSpace && !isWhitespace;
   },
   isValidCategoryName(input) {
+    if (typeof input !== 'string') {
+      return false;
+    }
+
     const isWhitespace = input.replace(/\s/g, '') === '';
     const isAlreadyAList = LocalStorage.getCustomListNames().some(listName => listName === input);
 
