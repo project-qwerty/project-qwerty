a7dba8b8cc89a02743109312865ec16a6abbcdd0
William Holt <halbrd@gmail.com>
2022-03-04 12:14:52 +1100

    implement Keyboard page layout with placeholders

--- src/views/Keyboard.vue
+++ src/views/Keyboard.vue
@@ -1,339 +1,182 @@
 <template>
-  <div id="keyboard" v-bind:style="{ 'text-transform': wordDisplayCapitalization }">
+  <main>
+    <header>
+      <IconButton
+          icon="x"
+          v-on:click="$router.back()" />
 
-    <div class="top-area-container">
-      <router-link to="/select-lists">
-        <font-awesome-icon class="top-area-back-button" icon="chevron-left"/>
-      </router-link>
-      <Progress :total="wordsPerSession" :current="this.count" />
-      <font-awesome-icon v-on:click="rehide" class="top-area-eye" v-bind:class="{ 'hidden': !isHidden }" icon="eye"/>
+      <div>1 / 5</div>
+    </header>
+
+    <div class="readout">
+      <div class="target">Beagle</div>
+      <div class="input">Beagle</div>
     </div>
 
-    <div>
-      <WordList v-bind:wordlist="wordlist" v-bind:index="currentWordIndex" class="wordlist" v-bind:class="{ 'hidden': isHidden }" />
-      <Output :output="output" />
-      <FinishedSessionOverlay v-if="showFinishedSessionOverlay" />
-      <NextWordOverlay v-if="showNextWordOverlay" v-on:update:buttonClicked="goToNextWord" />
-      <div class=keyboard>
-        <Keyboard :enabledCharacters="enabledCharacters" v-on:update:keypressed="keypressed" />
+    <div class="keyboard">
+      <div class="row row-q">
+        <div class="key"
+            v-for="letter in letters[0]" v-bind:key="letter">
+          {{ letter }}
+        </div>
+        <div class="key backspace">
+          <font-awesome-icon
+              class="backspace-icon"
+              icon="delete-left" />
+        </div>
+      </div>
+      <div class="row row-a">
+        <div class="key"
+            v-for="letter in letters[1]" v-bind:key="letter">
+          {{ letter }}
+        </div>
+      </div>
+      <div class="row row-z">
+        <div class="key"
+            v-for="letter in letters[2]" v-bind:key="letter">
+          {{ letter }}
+        </div>
+      </div>
+      <div class="row row-space">
+        <div class="key space"></div>
       </div>
     </div>
 
-  </div>
+  </main>
 </template>
 
 
 <script>
-  import BuiltInWordLists from '@/functions/BuiltInWordLists.js';
-  import LocalStorage from '@/functions/LocalStorage.js';
+  // import BuiltInWordLists from '@/functions/BuiltInWordLists.js';
+  // import LocalStorage from '@/functions/LocalStorage.js';
 
-  import Keyboard from '../components/keyboard/KeyboardComponent';
-  import WordList from '../components/WordList';
-  import FinishedSessionOverlay from '../components/FinishedSessionOverlay';
-  import NextWordOverlay from '@/components/NextWordOverlay.vue';
-  import Progress from '../components/Progress';
-  import Output from '@/components/Output.vue';
+  import IconButton from '@/components/IconButton.vue';
 
   export default {
-    name: 'app',
     components: {
-      WordList,
-      Keyboard,
-      FinishedSessionOverlay,
-      NextWordOverlay,
-      Progress,
-      Output,
+      IconButton,
     },
     data() {
       return {
-        timer: null,
-        wordlist: [],
-        currentWordIndex: 0,
-        count: 1,  // this is basically just currentWordIndex + 1 at all times and probably should be refactored out
-        showFinishedSessionOverlay: false,
-        showNextWordOverlay: false,
-        output: '',
-        isHidden: false,
-        wordsPerSession: null,
-        clickForNextWord: null,
-        wordRepetitions: 1,
-        repetitionsRemaining: 1,
-        key_pressed: false,
-        BuiltInWordLists: BuiltInWordLists,
-        assistanceLevel: null,
-        mistakeMade: false,
-        timerOnOff: false,  // Controls whether the timer is on or off
-        correct_audio: new Audio(require('@/assets/correct.mp3')),
-        wrong_audio: new Audio(require('@/assets/wrong_2.mp3')),
-        settings: {
-            timer: 5
-        },
-        wordDisplayCapitalization: null,
+        letters: [
+          ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+          ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+          ['z', 'x', 'c', 'v', 'b', 'n', 'm'],
+        ],
       }
-    },
-    // Variables to watch
-    watch: {
-      'currentWordIndex': function() {
-        if (this.count !== parseInt(this.wordsPerSession)) {
-          this.count += 1;
-        }
-      },
-    },
-
-    // Run on loading of page
-    created() {
-      this.wordRepetitions = LocalStorage.getSetting('wordRepetitions');
-
-      this.repetitionsRemaining = this.wordRepetitions;
-
-      // Word number control
-      this.wordsPerSession = LocalStorage.getSetting('wordsPerSession');
-
-      // Click to see next word
-      this.clickForNextWord = LocalStorage.getSetting('clickForNextWord');
-
-      // Timer Control
-      this.settings.timer = LocalStorage.getSetting('wordDisplayTime');
-      this.timerOnOff = this.settings.timer != 0;
-
-      this.timer = setTimeout(this.hide, this.settings.timer * 1000);
-
-      // Assistance Control
-      this.assistanceLevel = LocalStorage.getSetting('assistanceLevel');
-
-      // Text display capitalization case
-      this.wordDisplayCapitalization = LocalStorage.getSetting('wordDisplayCapitalization');
-
-      // Import custom lists
-      const selectedCustomLists = LocalStorage.getSelectedCustomListNames()
-          .map(listName => LocalStorage.getCustomListValidWords(listName));
-      const customWords = selectedCustomLists.flat();
-      this.wordlist = this.wordlist.concat(customWords);
-
-      this.inbuiltCreated(this.BuiltInWordLists);
-      this.wordlist = this.shuffleWordlist(this.wordlist);
-    },
-    computed: {
-      'assistanceOn': function() {
-        if (this.assistanceLevel === 'NONE') {
-          return false;
-        }
-
-        if (this.assistanceLevel === 'MAX') {
-          return true;
-        }
-
-        return this.mistakeMade;
-      },
-      'enabledCharacters': function() {
-        if (this.assistanceOn) {
-          return this.wordlist[this.currentWordIndex][this.output.length];
-        }
-        return 'abcdefghijklmnopqrstuvwxyz backspace';
-      },
-    },
-    updated: function () {
-      this.$nextTick(function () {
-        // Code that will run only after the entire view has been re-rendered
-        if (!this.key_pressed) {
-          return;
-        }
-        this.key_pressed = false;
-
-        var target_word = this.wordlist[this.currentWordIndex];
-
-        // If they aren't finished yet
-        if (this.output.length !== target_word.length) {
-          return;
-        }
-
-        // If they got the word wrong
-        if (this.output !== target_word) {
-          this.wrong_audio.play();
-          return;
-        }
-
-        // They got the word correct
-
-        this.correct_audio.play();
-        this.isHidden = false;
-
-        if (this.repetitionsRemaining == 1) {
-          // move to next word
-          this.currentWordIndex += 1;
-          this.output = '';
-          this.repetitionsRemaining = this.wordRepetitions;
-        } else {
-          // decrement repetitions
-          // note: this branch appears to not get reached;
-          //  instead, this decrementing happens in goToNextWord
-          //  gonna leave it alone for now as it should get picked up and fixed
-          //  in forthcoming refactors
-          this.repetitionsRemaining -= 1;
-        }
-
-        clearTimeout(this.timer);
-        this.timer = setTimeout(this.hide, this.settings.timer * 1000);
-
-        // If they finished the trials
-        if (this.count == this.wordsPerSession && this.repetitionsRemaining == this.wordRepetitions) {
-          this.currentWordIndex = 0;
-          this.output = '';
-          this.isHidden = true;
-          this.showFinishedSessionOverlay = true;
-        }
-
-        // If they finished the wordlist
-        if (this.currentWordIndex == this.wordlist.length) {
-          this.currentWordIndex = 0;
-        }
-      })
     },
     methods: {
-      rehide() {
-        this.isHidden = false;
-        this.timer = setTimeout(this.hide, this.settings.timer * 1000);
-      },
-      inbuiltCreated(wordlists) {
-        const selectedBuiltInLists = LocalStorage.getSelectedBuiltInListNames();
-        for (let listName of selectedBuiltInLists) {
-          this.wordlist = this.wordlist.concat(wordlists[listName].list);
-        }
-      },
-      shuffleWordlist(array) {
-        var currentIndex, temp, randomIndex;
-        currentIndex = array.length;
-        // While there remain elements to shuffle...
-        while (0 !== currentIndex) {
-          // Pick a remaining element...
-          randomIndex = Math.floor(Math.random() * currentIndex);
-          currentIndex -= 1;
 
-          // And swap it with the current element.
-          temp = array[currentIndex];
-          array[currentIndex] = array[randomIndex];
-          array[randomIndex] = temp;
-        }
-        return array;
-      },
-      goToNextWord() {
-        this.showNextWordOverlay = false;
-        this.output = '';
-
-        if (this.repetitionsRemaining == 1) {
-          this.currentWordIndex += 1;
-          this.repetitionsRemaining = this.wordRepetitions;
-        } else {
-          this.repetitionsRemaining -= 1;
-        }
-
-        clearTimeout(this.timer);
-        this.timer = setTimeout(this.hide, this.settings.timer * 1000);
-
-        // If they finished the trials
-        if (this.count == this.wordsPerSession && this.repetitionsRemaining == this.wordRepetitions) {
-          this.currentWordIndex = 0;
-          this.output = '';
-          this.isHidden = true;
-          this.showFinishedSessionOverlay = true;
-        }
-
-        // If they finished the wordlist
-        if (this.currentWordIndex == this.wordlist.length) {
-          this.currentWordIndex = 0;
-        }
-      },
-      keypressed(char) {
-        let targetChar = this.wordlist[this.currentWordIndex][this.output.length];
-        this.mistakeMade = char !== targetChar;
-
-        // on MIN assistance, if a wrong letter is typed, we shouldn't process the keystroke, other than to set mistakeMade
-        if (this.assistanceLevel === 'MIN' && this.mistakeMade) {
-          return;
-        }
-
-        // If button pressed wasn't backspace
-        if (char !== 'backspace') {
-          this.output += char;
-        } else {
-          // If button pressed was backspace
-          this.output = this.output.substring(0, this.output.length - 1);
-        }
-
-        // If they got the word correct
-        if (this.output === this.wordlist[this.currentWordIndex]) {
-          this.correct_audio.play();
-          this.isHidden = false;
-
-          // show NextWordOverlay, unless clickForNextWord is off, or there isn't a next word
-          let noMoreWordsLeft = this.currentWordIndex + 1 === this.wordsPerSession;
-          if (this.clickForNextWord && !noMoreWordsLeft) {
-            // in this case, goToNextWord will be called when the "Next word" alert button is clicked
-            this.showNextWordOverlay = true;
-          } else {
-            // in this case, it's called straight away
-            this.goToNextWord()
-          }
-
-          return;
-        }
-
-        // If they got the word wrong
-        if (this.output.length === this.wordlist[this.currentWordIndex].length) {
-          this.wrong_audio.play();
-          return;
-        }
-      },
-      hide() {
-        this.isHidden = this.timerOnOff;
-      }
     },
   }
 </script>
 
-<style scoped>
 
-  .top-area-container {
-    position: relative;
+<style scoped>
+  main {
+    height: 100%;
 
     display: flex;
+    flex-direction: column;
     justify-content: space-between;
   }
 
-  .top-area-back-button {
-    text-decoration: none;
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
-    font-size: 50px;
-    color: rgba(142, 142, 147);
+    border-bottom: solid 2px var(--faint-colour);
+
+    padding-top: 16px;
+    padding-bottom: 16px;
+    padding-left: 24px;
+    padding-right: 24px;
+
+    font-size: 24px;
   }
 
-  .top-area-eye {
-    font-size: 40px;
-    color: rgb(48, 209, 88);
-  }
-
-  .hidden {
-    visibility: hidden;
-  }
-
-  .btn {
-    display: inline-block;
-    border: none;
-    background: #555;
-    color: #fff;
-    padding: 7px 20px;
-    cursor: pointer;
-  }
-
-  .wordlist {
-    font-size: 70px;
-    text-decoration: none;
-    height: 100px;
-    color:rgba(72, 72, 74, 0.8);
+  .readout {
     font-weight: bold;
   }
 
+  .readout > .target {
+    color: var(--primary-colour);
+
+    font-size: 48px;
+
+    margin-bottom: 10px;
+  }
+
+  .readout > .input {
+    font-size: 96px;
+  }
+
+  .readout > .input:after {
+    /* add the underline */
+    content: '';
+    display: block;
+    margin: 0 auto;
+    width: 50%;
+    border-bottom: 1px solid var(--faint-colour);
+  }
+
   .keyboard {
-    bottom: 80px;
+    height: 40%;
+    padding-top: 40px;
+
+    background-color: var(--faint-colour);
+
+    font-size: 20px;
+  }
+
+  .keyboard > .row {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+
+    margin-bottom: 0.5em;
+    gap: 0.5em;
+  }
+
+  /* these are the styles that give you a natural keyboard */
+  /* .keyboard > .row-a {
+    padding-right: 8em;
+  }
+
+  .keyboard > .row-z {
+    padding-right: 13em;
+  }
+
+  .keyboard > .row-space {
+    padding-right: 13em;
+  } */
+
+  .keyboard > .row-q {
+    padding-left: 2em;
+  }
+
+  .keyboard .key {
+    width: 4em;
+    height: 3em;
+    border-radius: 0.5em;
+
+    background-color: var(--background-colour);
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .keyboard .key.backspace {
+    width: 6em;
+  }
+
+  .keyboard .key .backspace-icon {
+    font-size: 36px;
+  }
+
+  .keyboard .key.space {
+    width: 31em;
   }
 </style>
