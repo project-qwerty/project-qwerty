b7b7afdb3446e176954e8b1be9662d8d1b64f12f
William Holt <git@halbrd.com>
2022-10-24 21:54:37 +1100

    incorporate previous modal changes

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -2,14 +2,12 @@
   <div class="overlay"
       :class="{ hidden: !shown, }">
     <div ref="modal"
-        class="modal">
-      <div class="modal-inner"
-          :style="{
-            '--min-width': minWidth,
-            '--min-height': minHeight,
-          }">
-        <slot></slot>
-      </div>
+        class="modal"
+        :style="{
+          '--width': width,
+          '--height': height,
+        }">
+      <slot></slot>
     </div>
   </div>
 </template>
@@ -22,11 +20,11 @@
         type: Boolean,
         default: false,
       },
-      minWidth: {
+      width: {
         type: String,
         default: 'initial',
       },
-      minHeight: {
+      height: {
         type: String,
         default: 'initial',
       },
@@ -90,51 +88,20 @@
   }
 
   .modal {
-    max-width: 100%;
-    /* expand on mobile */
-    min-width: 90%;
-    max-height: 100%;
+    width: var(--width);
+    height: var(--height);
+    margin: var(--thin-gap);
+
+    box-sizing: border-box;
+    padding: 2em;
 
     border-radius: 1em;
-    margin: 1rem;
-
-    /* allow modal contents to scroll */
-    overflow: auto;
 
     background-color: var(--background-colour);
     color: var(--foreground-colour);
   }
 
-  .modal-inner {
-    border: 4px solid green;
-    --padding: 2em;
-
-    padding: var(--padding);
-
-    max-width: 500px;
-    min-width: calc(var(--min-width) - var(--padding) * 2);
-    min-height: calc(var(--min-height) - var(--padding) * 2);
-
-    display: flex;
-    flex-direction: column;
-  }
-
-  .modal-inner > * {
-    /* this is needed because of this issue: */
-    /* https://stackoverflow.com/questions/8468066/child-inside-parent-with-min-height-100-not-inheriting-height */
-    min-height: inherit;
-  }
-
   .hidden {
     display: none;
   }
-
-  /* make the h tags a bit smaller in the modal */
-  h1 { font-size: 1.5em; }
-  h2 { font-size: 1.3em; }
-  h3 { font-size: 1em; }
-  h4 { font-size: 0.8em; }
-  h5 { font-size: 0.7em; }
-  h6 { font-size: 0.6em; }
-
 </style>
