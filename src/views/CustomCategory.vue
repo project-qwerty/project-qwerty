e419fe14772ecd514e8d33c803a96dec46f16255
William Holt <git@halbrd.com>
2022-10-26 18:38:41 +1100

    restore intended styling

--- src/views/CustomCategory.vue
+++ src/views/CustomCategory.vue
@@ -11,16 +11,17 @@
 
       <header class="title-controls-header faint-border-bottom">
         <IconHeader
-            class="category-title"
             icon="list"
             :iconColour="Colours.stringToColour(categoryName)"
             :text="categoryName" />
+
         <div class="controls">
           <ActionButton
               class="new-word-button"
               icon="plus"
               text="New"
               v-on:click="clickAddWord" />
+
           <Dropdown
               class="options-menu"
               :options="[
@@ -32,22 +33,23 @@
         </div>
       </header>
 
-      <div
-          class="word-wrapper"
-          v-for="(word, index) in wordValues" v-bind:key="index">
+      <div class="faint-border-bottom">
+        <div
+            class="word-wrapper"
+            v-for="(word, index) in wordValues" v-bind:key="index">
 
-        <input
-            class="qwerty-text-input"
-            :class="{ invalid: !Validation.isValidWord(word) }"
-            placeholder="new word"
-            v-model="wordValues[index]"
-            @input="updateWord(index)">
+          <input
+              class="qwerty-text-input"
+              :class="{ invalid: !Validation.isValidWord(word) }"
+              placeholder="new word"
+              v-model="wordValues[index]"
+              @input="updateWord(index)">
 
-        <IconButton
-            class="delete-word-button"
-            icon="x"
-            v-on:click="clickDeleteWord(index)" />
+          <IconButton
+              icon="x"
+              v-on:click="clickDeleteWord(index)" />
 
+        </div>
       </div>
     </div>
 
@@ -231,8 +233,9 @@
     font-size: 24px;
   }
 
-  .delete-word-button {
-    margin-left: 1rem;
+  /* this turns the final option (delete) red */
+  /deep/ .options-menu .dropdown-item:last-of-type {
+    color: var(--negative-colour);
   }
 
   .options-menu {
