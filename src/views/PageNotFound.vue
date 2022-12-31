8e52c14957052440f430b46481b3bfe76751d5e2
TimHarris96 <59544597+TimHarris96@users.noreply.github.com>
2022-12-31 16:21:00 +1100

    we did it!

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,28 +1,14 @@
 <template>
-  <div class="center">
     <main>
-      <div class="header">
         <h1>Oops!</h1>
-      </div>
       <p>This page does not exist. <router-link to="/" >Click here</router-link> to return to Project
         QWERTY.</p>
     </main>
-  </div>
 </template>
 
 
 <style scoped>
 main {
-  flex-direction: column;
-}
-
-.center {
-  display: flex;
-  justify-content: center;
-}
-
-.header {
-  display: flex;
-  justify-content: center;
+  text-align: center;
 }
 </style>
