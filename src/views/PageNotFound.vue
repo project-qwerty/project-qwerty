97968bb91d1977e061c0965b232148874e790007
TimHarris96 <59544597+TimHarris96@users.noreply.github.com>
2023-01-03 18:46:48 +1100

    formtatting :)

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,9 +1,9 @@
 <template>
-    <main>
-        <h1>Oops!</h1>
-      <p>This page does not exist. <router-link to="/" >Click here</router-link> to return to Project
-        QWERTY.</p>
-    </main>
+  <main>
+    <h1>Oops!</h1>
+    <p>This page does not exist. <router-link to="/">Click here</router-link> to return to Project
+      QWERTY.</p>
+  </main>
 </template>
 
 
