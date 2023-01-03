9d55b892eceb9c6d585749904b32edfc1565bd1c
William Holt <git@halbrd.com>
2023-01-03 23:00:45 +1100

    remove linebreak

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,8 +1,7 @@
 <template>
   <main>
     <h1>Oops!</h1>
-    <p>This page does not exist. <router-link to="/">Click here</router-link> to return to Project
-      QWERTY.</p>
+    <p>This page does not exist. <router-link to="/">Click here</router-link> to return to Project QWERTY.</p>
   </main>
 </template>
 
