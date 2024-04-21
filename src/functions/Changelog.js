d7c3efa0af31a869f23c9ba2f45a80a2cbbede63
William Holt <git@halbrd.com>
2024-04-21 23:25:42 +1000

    update changelog wording

--- src/functions/Changelog.js
+++ src/functions/Changelog.js
@@ -1,6 +1,6 @@
 export default {
   'TODO': [
-    'Made page headers with controls stick to the top of the screen',
+    'Made page headers with controls stick to the top of the screen on large devices',
     'Moved controls to the bottom of the screen on mobile',
     'Made the New Category, Rename Category, and New Word buttons automatically focus their relevant text fields',
   ],
